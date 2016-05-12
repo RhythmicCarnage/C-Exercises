869a53be8d0d2f456095ea34ff03025e20485a50
Jeremy Koepke <JeremyKoepke@JeremyKoepke.com>
2016-05-12 15:33:15 -0600

    Update

---
+++ 2.1.cpp
@@ -0,0 +1,17 @@
+/*
+    Jeremy Koepke
+    Section 2.1
+    MIT OpenCourseWare
+    Assignment 1
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    const char *greeting = "Hello, World!";
+    cout << *greeting;
+
+    return 0;
+}
